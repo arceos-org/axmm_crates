4e58d27aad951d30b49bbd334436b5c886a35c13
aarkegz <aarkegz@gmail.com>
2024-08-20 17:15:45 +0800

    fix docs

--- memory_set/src/backend.rs
+++ memory_set/src/backend.rs
@@ -1,15 +1,18 @@
 use memory_addr::MemoryAddr;
 
 /// Underlying operations to do when manipulating mappings within the specific
-/// [`MemoryArea`].
+/// [`MemoryArea`](crate::MemoryArea).
 ///
 /// The backend can be different for different memory areas. e.g., for linear
 /// mappings, the target physical address is known when it is added to the page
 /// table. For lazy mappings, an empty mapping needs to be added to the page table
 /// to trigger a page fault.
 pub trait MappingBackend: Clone {
+    /// The address type used in the memory area.
     type Addr: MemoryAddr;
+    /// The flags type used in the memory area.
     type Flags: Copy;
+    /// The page table type used in the memory area.
     type PageTable;
 
     /// What to do when mapping a region within the area with the given flags.
