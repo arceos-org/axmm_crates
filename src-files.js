84882dc2ffb86d9d8294165b19a2421f25337983
equation314 <equation618@gmail.com>
2025-03-07 04:23:35 +0000

    Deploying to gh-pages from @ arceos-org/axmm_crates@87b8ebcd52a2f248e20c847130ff20614bc64660 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["memory_addr",["",[],["addr.rs","iter.rs","lib.rs","range.rs"]]],["memory_set",["",[],["area.rs","backend.rs","lib.rs","set.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[65,66]}
---
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["memory_addr",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"memory_addr/struct.PhysAddr.html\" title=\"struct memory_addr::PhysAddr\">PhysAddr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"memory_addr/struct.VirtAddr.html\" title=\"struct memory_addr::VirtAddr\">VirtAddr</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[765]}
